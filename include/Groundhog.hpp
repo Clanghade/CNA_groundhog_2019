ff25d9f7e38a4fecf193a0ef2c29f9a89a8d1852
Clanghade <48475282+Clanghade@users.noreply.github.com>
2020-04-07 09:12:37 +0200

    Add folders

---
+++ include/Groundhog.hpp
@@ -0,0 +1,49 @@
+/*
+** EPITECH PROJECT, 2020
+** Groundhog
+** File description:
+** Groundhog
+*/
+
+#ifndef GROUNDHOG_HPP_
+#define GROUNDHOG_HPP_
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+#include <math.h>
+#include <utility>
+
+class Groundhog {
+    public:
+        Groundhog();
+        ~Groundhog();
+
+        void displayData(void);
+        void computeIncrease(void) const;
+        void computeRelative(void);
+        void computeStandard(void);
+        double computeAverage(void) const;
+
+        void setPeriod(unsigned int newPeriod);
+        void addInput(const std::string &newInput);
+
+        unsigned int getPeriod(void) const;
+        std::vector<double> getInputs(void) const;
+
+    protected:
+    private:
+        double _previousValue;
+        double _relativeNdays;
+        unsigned int _period;
+        std::vector<double> _values;
+        std::vector<double> _increase;
+        int _lastR;
+        bool _isSwitch;
+        int _switch;
+        std::vector<std::pair<double, double>> _weird;
+};
+
+#endif /* !GROUNDHOG_HPP_ */
